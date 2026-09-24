bf11e14fa5f755eef72a0e42b012ec2eab0de1af
tor.wallberg <tor.wallberg@5CD4153M3G>
2026-09-24 13:52:16 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace tomtens_verkstad
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
